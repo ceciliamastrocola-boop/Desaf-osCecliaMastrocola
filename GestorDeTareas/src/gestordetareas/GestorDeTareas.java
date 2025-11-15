5bcb1f501f30250af2bec856da836dbb4ae86ec6
ceciliamastrocola@gmail.com <ceciliamastrocola@gmail>
2025-11-15 10:41:11 -0300

    ADD_paquetesMVC

---
+++ GestorDeTareas/src/gestordetareas/GestorDeTareas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gestordetareas;
+
+/**
+ *
+ * @author Ceciia
+ */
+public class GestorDeTareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
